dd1e24fa6217d48b9603a07a28f1a66bd164e530
Ildeberto <ildeberto@MacBook.local>
2026-02-24 00:01:34 -0600

    Update derivation formatting

--- api/index.py
+++ api/index.py
@@ -37,7 +37,7 @@ INDEX_HTML = r"""
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Derivación por Definición</title>
+    <title>Derivación paso a paso</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&family=IBM+Plex+Mono:wght@500&display=swap" rel="stylesheet" />
